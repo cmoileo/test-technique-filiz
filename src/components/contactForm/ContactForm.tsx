8b17d97daa8ef252ff81673d610092be1da44b5e
cmoileo <106542483+cmoileo@users.noreply.github.com>
2024-03-23 13:53:05 +0100

    improve form validation

--- src/components/contactForm/ContactForm.tsx
+++ src/components/contactForm/ContactForm.tsx
@@ -1,29 +1,48 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
 const ContactForm = () => {
+    const { register, handleSubmit, formState } = useForm({ defaultValues: {} });
+    const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+
+    const { errors } = formState;
+
+    const onSubmit = () => {
+        setIsFormSubmitted(true);
+    };
+
     return (
-        <form className="contact-form" action="action">
+        <form className="contact-form" onSubmit={handleSubmit(onSubmit)} action="action">
             <div className="wrapper">
                 <div className="container">
-                    <label className="p-s" htmlFor="Nom">Nom</label>
-                    <input className="p-m" type="text" id="Nom" name="Nom" required/>
+                    <label className="p-s" htmlFor="lastName">Nom de famille</label>
+                    <input className="p-m" type="text" id="lastName" {...register('lastName', { required: 'Le nom de famille est requis' })} aria-label="Nom de famille" />
+                    {errors?.lastName && <span className="error">{errors.lastName.message}</span>}
                 </div>
                 <div className="container">
-                    <label className="p-s" htmlFor="Prénom">Prénom</label>
-                    <input className="p-m" type="text" id="Prénom" name="Prénom" required/>
+                    <label className="p-s" htmlFor="firstName">Prénom</label>
+                    <input className="p-m" type="text" id="firstName" {...register('firstName', { required: 'Le prénom est requis' })} aria-label="Prénom" />
+                    {errors?.firstName && <span className="error">{errors.firstName.message}</span>}
                 </div>
             </div>
             <div className="wrapper">
                 <div className="container">
-                    <label className="p-s" htmlFor="Telephone">N° de téléphone</label>
-                    <input className="p-m" type="tel" id="Telephone" name="Telephone" required/>
+                    <label className="p-s" htmlFor="phoneNumber">Numéro de téléphone</label>
+                    <input className="p-m" type="tel" id="phoneNumber" {...register('phoneNumber', { required: 'Le numéro de téléphone est requis', pattern: { value: /^0[1-9]([-. ]?[0-9]{2}){4}$/, message: 'Le numéro de téléphone est invalide' } })} />
+                    {errors?.phoneNumber && <span className="error">{errors.phoneNumber.message}</span>}
                 </div>
                 <div className="container">
-                    <label className="p-s" htmlFor="Email">Adresse email</label>
-                    <input className="p-m" type="email" id="Email" name="Email" required/>
+                    <label className="p-s" htmlFor="email">Adresse e-mail</label>
+                    <input className="p-m" type="email" id="email" {...register('email', { required: 'L\'adresse e-mail est requise', pattern: { value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, message: 'L\'adresse e-mail est invalide' } })} />
+                    {errors?.email && <span className="error">{errors.email.message}</span>}
                 </div>
             </div>
-            <button className="main-cta" type="submit">Envoyer</button>
+            {isFormSubmitted && !errors?.lastName && !errors?.firstName && !errors?.phoneNumber && !errors?.email && (
+                <p className="p-l color-main-green success-msg">Formulaire soumis avec succès !</p>
+            )}
+            <button className="main-cta" type="submit" aria-label="Envoyer le formulaire">Envoyer</button>
         </form>
     );
-}
+};
 
 export default ContactForm;
